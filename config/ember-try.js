5c687ad9727460c633d66e555d13178383dca8c0
Katie Gengler <katie@kmg.io>
2019-01-10 18:24:43 -0500

    Update to 3.7

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,106 +6,27 @@ module.exports = function() {
   return Promise.all([
     getChannelURL('release'),
     getChannelURL('beta'),
-    getChannelURL('canary'),
+    getChannelURL('canary')
   ]).then((urls) => {
     return {
-      useYarn: true,
       scenarios: [
         {
-          name: 'Ember 1.12.1',
-          bower: {
-            dependencies: {
-              'ember': '1.12.1'
-            }
+          name: 'ember-lts-2.18',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
           },
           npm: {
             devDependencies: {
-              'ember-source': null,
-              'ember-native-dom-event-dispatcher': null
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~2.18.0'
             }
           }
         },
         {
-          name: 'Ember 1.13',
-          bower: {
-            dependencies: {
-              'ember': '~1.13.0'
-            }
-          },
+          name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
-              'ember-source': null,
-              'ember-native-dom-event-dispatcher': null
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.4',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-4'
-            },
-            resolutions: {
-              'ember': 'lts-2-4'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-source': null,
-              'ember-native-dom-event-dispatcher': null
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-8'
-            },
-            resolutions: {
-              'ember': 'lts-2-8'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-source': null,
-              'ember-native-dom-event-dispatcher': null
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.12.0',
-              'ember-native-dom-event-dispatcher': null
-            },
-          }
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          allowedToFail: true,
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2],
-              'ember-native-dom-event-dispatcher': null
-            }
-          }
-        },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1],
-              'ember-native-dom-event-dispatcher': null
+              'ember-source': '~3.4.0'
             }
           }
         },
@@ -118,16 +39,41 @@ module.exports = function() {
           }
         },
         {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[1]
+            }
+          }
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[2]
+            }
+          }
+        },
+        // The default `.travis.yml` runs this scenario via `npm test`,
+        // not via `ember try`. It's still included here so that running
+        // `ember try:each` manually or from a customized CI config will run it
+        // along with all the other scenarios.
+        {
           name: 'ember-default',
           npm: {
             devDependencies: {}
           }
         },
         {
-          name: 'ember-default-jquery',
+          name: 'ember-default-with-jquery',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': true
+            })
+          },
           npm: {
             devDependencies: {
-              'ember-native-dom-event-dispatcher': null
+              '@ember/jquery': '^0.5.1'
             }
           }
         }
